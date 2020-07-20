11f80b0f09ba31eed714e602ace6a7a29f370f2e
Daniil <pde-rnd@yandex.ru>
2020-07-20 12:08:54 +0300

    7.2:Добвлены тесты сообщений об ошибках при создании пользователя

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,6 +11,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }}
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
+    assert_select 'ul#error-list'
   end
   test "valid signup information" do
     get signup_path
